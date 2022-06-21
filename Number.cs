4f07f371c86b9314339827a7fb57274bc8140f4f
nidhisingh0209 <107402112+nidhisingh0209@users.noreply.github.com>
2022-06-21 20:25:49 +0530

    Create Number.cs

---
+++ Number.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment21
+{
+   public class Number
+    {
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+        public int number3 { get; set; }
+    }
+}
